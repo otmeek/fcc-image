2051176cce4f3cd84bd7598112f0f368da9e2dff
Mafalda Pereira <otmeek@gmail.com>
2016-03-03 16:24:48 +0000

    complete basejump requirements

--- server.js
+++ server.js
@@ -1,31 +1,105 @@
 'use strict';
 
+var mongo   = require('mongodb').MongoClient;
 var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
+var path    = require('path');
+var Imgur   = require('imgurjs');
+var imgur   = new Imgur({
+	clientId: 'd19c43080d4fa65',
+	clientSecret: 'bc2ecab586657c7f806c590a639d73214110c3e3'
+});
 
 var app = express();
 require('dotenv').load();
-require('./app/config/passport')(passport);
 
-mongoose.connect(process.env.MONGO_URI);
+app.use(express.static(path.join(__dirname, 'htdocs')));
 
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
+app.get('/imagesearch/:KEYWORDS', function(req, res) {
+	var keywords = req.params.KEYWORDS.split(' ').join(' AND ');
+	var offset   = req.query.offset;
+	if(offset === undefined)
+		offset = 1;
+		
+	// record query
+	mongo.connect('mongodb://localhost:27017/fccimage', function(err, db) {
+		if(err) throw err;
+		var collection = db.collection('query');
+		
+		collection.insert({
+			terms: keywords,
+			when : getDateTime()
+		}, function(err) {
+			if (err) throw err;
+			db.close();
+		});
+	});
+	
+	imgur.gallery.search({
+		page: offset,
+		q   : keywords
+	}).then(function(data) {
+		var arr = data.data;
+		var newArr = [];
+		var obj;
+		// send only img url, description and page url
+		arr.forEach(function(x) {
+			obj = {
+				id      : x.id,
+				title   : x.title,
+				link    : x.link,
+			}
+			newArr.push(obj);
+		});
+		if(newArr.length === 0) {
+			newArr.push('No results.');
+		}
+		res.send(newArr);
+	}).catch(function(error) {
+		if(error) throw error;
+	});
+	
+	function getDateTime() {
+	
+	    var date = new Date();
+	
+	    var hour = date.getHours();
+	    hour = (hour < 10 ? "0" : "") + hour;
+	
+	    var min  = date.getMinutes();
+	    min = (min < 10 ? "0" : "") + min;
+	
+	    var sec  = date.getSeconds();
+	    sec = (sec < 10 ? "0" : "") + sec;
+	
+	    var year = date.getFullYear();
+	
+	    var month = date.getMonth() + 1;
+	    month = (month < 10 ? "0" : "") + month;
+	
+	    var day  = date.getDate();
+	    day = (day < 10 ? "0" : "") + day;
+	
+	    return year + "-" + month + "-" + day + " " + hour + ":" + min + ":" + sec;
+	
+	}
+});
 
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
+app.get('/api/latest/imagesearch', function(req, res) {
+	mongo.connect('mongodb://localhost:27017/fccimage', function(err, db) {
+		if(err) throw err;
+		var collection = db.collection('query');
+		
+		collection.find({}, {
+			_id  : 0,
+			terms: 1,
+			when : 1
+		}).toArray(function(err, docs) {
+			if(err) throw err;
+			res.send(docs);
+			db.close();
+		});
+	});
+});
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
